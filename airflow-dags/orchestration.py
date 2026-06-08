373954e14c23814727025c0c3335074fc8d9f2f8
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2026-06-08 11:22:41 +0100

    Updated the airflow orchestration file

--- airflow-dags/orchestration.py
+++ airflow-dags/orchestration.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.smtp.notifications.smtp import send_smtp_notification
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
-AIRBYTE_CONN_ID_FROM_ENV = os.getenv("AIRBYTE_CONNECTION_ID")
 
+AIRBYTE_CONN_ID_FROM_ENV = os.getenv("AIRBYTE_CONNECTION_ID")
 
 default_args = {
     'owner': 'airflow',
@@ -18,9 +18,39 @@ with DAG(
     start_date=datetime(2026, 5, 10),
     schedule="@daily",
     default_args=default_args,
-    catchup=False
-) as dag:
+    catchup=False,
+    on_failure_callback=[
+        send_smtp_notification(
+            smtp_conn_id="smtp_default",
+            from_email="fakayodeoluseguno@gmail.com",
+            to="olukayodeoluseguno@gmail.com",
+            subject="Error❌:  The Dag {{ dag.dag_id }} Failed - {{ds}}",
+            html_content="""
+                                <p><b>DAG:</b> {{ dag.dag_id }}</p>
+                                <p><b>Run ID:</b> {{ run_id }}</p>
+                                <p><b>Execution Date:</b> {{ ds }}</p>
+                                <p><b>Status:</b> Failed</p>
+                            """,
+        )
+    ],
 
+    on_success_callback=[
+        send_smtp_notification(
+            smtp_conn_id="smtp_default",
+            from_email="fakayodeoluseguno@gmail.com",
+            to="olukayodeoluseguno@gmail.com",
+            subject="Success✅:  The Dag {{ dag.dag_id }} Succeeded - {{ds}}",
+            html_content="""
+                                <p><b>DAG:</b> {{ dag.dag_id }}</p>
+                                <p><b>Run ID:</b> {{ run_id }}</p>
+                                <p><b>Execution Date:</b> {{ ds }}</p>
+                                <p><b>Status:</b> Succeeded</p>
+                            """,
+        )
+    ]
+    
+) as dag:
+    
     sync_data = AirbyteTriggerSyncOperator(
         task_id="sync_airbyte_connection",
         airbyte_conn_id="airbyte_connection",
